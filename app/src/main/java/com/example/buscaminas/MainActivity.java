b73c15da5f77c4438f299bc808cb4d98db0db223
mikelmatilla <mikel.matilla1@gmail.com>
2021-11-05 11:38:19 +0100

    Navegacion entre Activitys e insercion de datos en la tabla hecho

--- app/src/main/java/com/example/buscaminas/MainActivity.java
+++ app/src/main/java/com/example/buscaminas/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.buscaminas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/buscaminas/ScoreActivity.java
+++ app/src/main/java/com/example/buscaminas/ScoreActivity.java
@@ -1,7 +1,88 @@
 package com.example.buscaminas;
 
-public class ScoreActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
+public class ScoreActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView titleTxt;
+    private ImageView scoreImg;
+    private TableLayout table;
+    private Button playAgainBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        titleTxt = (TextView) findViewById(R.id.textView);
+        titleTxt.setText(getString(R.string.score));
+
+        scoreImg = (ImageView) findViewById(R.id.imageView);
+
+        for (int i = 0; i <= 15; i++) {
+            String username = "Jugandor" + String.valueOf(i);
+            String score = String.valueOf(i);
+            table = (TableLayout) findViewById(R.id.tableLayout);
+            TableRow row = new TableRow(this);
+            TextView textview1 = new TextView(this);
+            TextView textview2 = new TextView(this);
+
+            //Añadir username
+            textview1.setText(username);
+            textview1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            textview1.setTextSize(20);
+            textview1.setBackground(getDrawable(R.drawable.border));
+
+            //Añadir score
+            textview2.setText(score);
+            textview2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            textview2.setTextSize(20);
+            textview2.setBackground(getDrawable(R.drawable.border));
+
+            row.addView(textview1);
+            row.addView(textview2);
+            table.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        }
+        String username = "Jugandor n";
+        String score = "002";
+        table = (TableLayout) findViewById(R.id.tableLayout);
+        TableRow row = new TableRow(this);
+        TextView textview1 = new TextView(this);
+        TextView textview2 = new TextView(this);
+
+        //Añadir username
+        textview1.setText(username);
+        textview1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        textview1.setTextSize(20);
+        textview1.setBackground(getDrawable(R.drawable.border));
+
+        //Añadir score
+        textview2.setText(score);
+        textview2.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        textview2.setTextSize(20);
+        textview2.setBackground(getDrawable(R.drawable.border));
+
+        row.addView(textview1);
+        row.addView(textview2);
+        table.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+        playAgainBtn = (Button) findViewById(R.id.button);
+        playAgainBtn.setText(getString(R.string.play_again));
+        playAgainBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
